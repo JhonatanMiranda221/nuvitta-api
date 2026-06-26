f52ea22cda5afea6991b1bd2ebc730062d5b4c56
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-06-26 11:41:34 -0300

    feat: módulo paciente completo

--- src/paciente/paciente.module.ts
+++ src/paciente/paciente.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Paciente } from "./entities/paciente.entity";
+import { PacienteService } from "./services/paciente.service";
+import { PacienteController } from "./controllers/paciente.controller";
+import { Nutricionista } from "../nutricionista/entities/nutricionista.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Paciente])],
-    controllers: [],
-    providers: [],
+    imports: [TypeOrmModule.forFeature([Paciente, Nutricionista])],
+    controllers: [PacienteController],
+    providers: [PacienteService],
 })
 export class PacienteModule {}
---
+++ src/paciente/services/paciente.service.ts
@@ -0,0 +1,132 @@
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from "@nestjs/common";
+
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository, ILike } from "typeorm";
+
+import { Paciente } from "../entities/paciente.entity";
+import { CreatePacienteDto } from "../Dto/create-paciente.dto";
+import { UpdatePacienteDto } from "../Dto/update-paciente.dto";
+import { Nutricionista } from "../../nutricionista/entities/nutricionista.entity";
+
+
+@Injectable()
+export class PacienteService {
+
+  constructor(
+  @InjectRepository(Paciente)
+  private pacienteRepository: Repository<Paciente>,
+
+  @InjectRepository(Nutricionista)
+  private nutricionistaRepository: Repository<Nutricionista>,
+) {}
+
+
+  async create(dto: CreatePacienteDto): Promise<Paciente> {
+
+    const emailExistente = await this.pacienteRepository.findOne({
+      where: {
+        email: dto.email,
+      },
+    });
+
+
+    if (emailExistente) {
+      throw new ConflictException(
+        "Email já cadastrado"
+      );
+    }
+
+
+    const nutricionista = await this.nutricionistaRepository.findOne({
+      where: {
+        id: dto.nutricionistaId,
+      },
+    });
+
+
+    if (!nutricionista) {
+      throw new NotFoundException(
+        "Nutricionista não encontrado"
+      );
+    }
+
+
+    const paciente = this.pacienteRepository.create(dto);
+
+
+    paciente.nutricionista = nutricionista;
+
+
+    return await this.pacienteRepository.save(paciente);
+  }
+
+
+  async findAll(): Promise<Paciente[]> {
+
+    return await this.pacienteRepository.find({
+      relations: ['nutricionista']
+    });
+  }
+
+
+  async findById(id: number): Promise<Paciente> {
+
+    const paciente = await this.pacienteRepository.findOne({
+      where: { id },
+      relations: ['nutricionista']
+    });
+
+
+    if (!paciente) {
+      throw new NotFoundException("Paciente não encontrado");
+    }
+
+
+    return paciente;
+  }
+
+
+  async findByNome(nome: string): Promise<Paciente[]> {
+
+    return await this.pacienteRepository.find({
+      where: {
+        nome: ILike(`%${nome}%`),
+      },
+      relations: ['nutricionista']
+    });
+
+  }
+
+
+  async update(
+    id: number,
+    dto: UpdatePacienteDto,
+  ): Promise<Paciente> {
+
+
+    const paciente = await this.findById(id);
+
+
+    Object.assign(paciente, dto);
+
+
+    return await this.pacienteRepository.save(paciente);
+
+  }
+
+
+  async delete(id: number): Promise<void> {
+
+
+    const paciente = await this.findById(id);
+
+
+    await this.pacienteRepository.remove(paciente);
+
+  }
+
+}
